96507e15f0d3aa78764fe79ff82416916160a148
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2025-06-12 01:07:46 -0700

    adding logic for generator config file parsing.

--- CSM Dev/CSM Dev/Commands/GenCommand.cs
+++ CSM Dev/CSM Dev/Commands/GenCommand.cs
@@ -1,6 +1,16 @@
 ﻿using System.CommandLine;
 
+using ShellProgressBar;
+
 namespace CSM_Dev.Commands;
+
+enum SourceMirrorsLanguage {
+    csharp,
+}
+
+enum TargetMirrorsLanguage {
+    dart,
+}
 
 /// <summary>
 ///     {command} class.
@@ -9,14 +19,50 @@ namespace CSM_Dev.Commands;
 ///     language files based on the source solution entities, services and sources files to convert.
 /// </summary>
 public class GenCommand
-    : Command
-{
-    public GenCommand() 
+    : Command {
+
+    /// <summary>
+    ///     Creates a new <see cref="GenCommand"/> instance.
+    /// </summary>
+    public GenCommand()
         : base(
         "gen",
     "Generates the mirror from a .Net Server solution entities, enumerators and services to the target development platform"
         ) {
 
 
+        Option<string> configFileNameOption = new(
+            "--config-file-name",
+                description: "Specifies a mirror configuration file name for the generation (match case sensitive)",
+                getDefaultValue: () => ".mirror_config.json"
+            );
+
+        AddOption(configFileNameOption);
+
+        this.SetHandler(
+               (string configFileName) => {
+
+                   using ProgressBar progressBar = new(
+                           2,
+                           "Starting mirrors generation process...",
+                           new ProgressBarOptions {
+                               ForegroundColor = ConsoleColor.DarkCyan,
+                               ProgressCharacter = '-',
+                               EnableTaskBarProgress = true,
+                           }
+                       );
+
+                   progressBar.Tick("Getting configuration file...");
+
+                   DirectoryInfo workDir = new(Directory.GetCurrentDirectory());
+                   FileInfo[] workDirFiles = workDir.GetFiles();
+
+                   FileInfo configFileInfo = workDirFiles.FirstOrDefault(workDirFile => workDirFile.Name == configFileName) 
+                        ?? throw new FileNotFoundException($"File ({configFileName}) not found at ({workDir.FullName}) working directory");
+
+
+               },
+               configFileNameOption
+            );
     }
 }
