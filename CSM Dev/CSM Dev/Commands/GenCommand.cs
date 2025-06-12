9f2f80bac6c4776a4d4cfd7d022b8e598041b9c3
Juan Renato Urrea Ortiz <UsuaryRenato@hotmail.com>
2025-06-11 22:50:53 -0700

    added git igore and starting with gen command

---
+++ CSM Dev/CSM Dev/Commands/GenCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.CommandLine;
+
+namespace CSM_Dev.Commands;
+
+/// <summary>
+///     {command} class.
+///     
+///     Implements the {gen} command at the command-line tools, wich generates target solution
+///     language files based on the source solution entities, services and sources files to convert.
+/// </summary>
+public class GenCommand
+    : Command
+{
+    public GenCommand() 
+        : base(
+        "gen",
+    "Generates the mirror from a .Net Server solution entities, enumerators and services to the target development platform"
+        ) {
+
+
+    }
+}
---
+++ CSM Dev/CSM Dev/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.CommandLine;
+
+internal class Program
+{
+
+
+    enum SourceMirrorsLanguage
+    {
+        csharp,
+    }
+
+    enum TargetMirrorsLanguage
+    {
+        dart,
+    }
+
+    private static async Task Main(string[] args)
+    {
+
+        Option<SourceMirrorsLanguage> sourceMirrorOption = new(
+                "--source-language",
+                description: "Specifies the source server solution to mirror language",
+                getDefaultValue: () => SourceMirrorsLanguage.csharp
+            );
+        
+        Option<DirectoryInfo> sourcePathOption = new(
+                "--source-path",
+                description: "Specifies the source server solution path to get files to mirror",
+                getDefaultValue: () => new DirectoryInfo(Directory.GetCurrentDirectory())
+            );
+
+
+        Option<TargetMirrorsLanguage> targetMirrorOption = new(
+                "--target-language",
+                description: "Specifies the target client solution to mirror language",
+                getDefaultValue: () => TargetMirrorsLanguage.dart
+            );
+
+        Command genCommand = new(
+            "gen",
+            "Generates the mirror from a .Net Server solution entities, enumerators and services to the target development platform"
+        ) {
+            sourceMirrorOption,
+            sourcePathOption,
+            targetMirrorOption,
+        };
+        genCommand.SetHandler(
+               (SourceMirrorsLanguage sourceMirror, DirectoryInfo sourcePath, TargetMirrorsLanguage targetMirror) =>
+               {
+                   Console.WriteLine($"sourceMirror {sourceMirror}");
+                   Console.WriteLine($"sourcePath {sourcePath}");
+                   Console.WriteLine($"targetMirror {targetMirror}");
+               },
+               sourceMirrorOption,
+               sourcePathOption, 
+               targetMirrorOption
+            );
+
+
+
+
+        RootCommand rootCommand = new("Command line tools for {CSM} developers to interact with development platforms easily")
+        {
+            genCommand,
+        };
+        rootCommand.Name = "csm_dev";
+        await rootCommand.InvokeAsync(args);
+    }
+}
