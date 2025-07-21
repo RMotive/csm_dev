76cda8c1e7783a39eacb6d03fd557e0a6db81be6
Renato Urrea <usuaryrenato@hotmail.com>
2025-07-21 16:31:02 -0700

    configuring package metadata

---
+++ CSM Dev/CSM Dev.Core/MirrorAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace CSM_Dev.Core;
+
+/// <summary>
+///     Represents an entity attribute in the CSM Dev framework, used to get Entity references for the mirror process,
+///     this entities are transpilated from soure languages to target languages.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+public class MirrorAttribute 
+    : Attribute {
+
+    /// <summary>
+    ///     Category name where this entity belongs to, this is used to group entities in the mirror process.
+    /// </summary>
+    public string Category { get; init; }
+
+    /// <summary>
+    ///     Creates a new instance.
+    /// </summary>
+    /// <param name="category">
+    ///     Category name where this entity belongs to, this is used to group entities in the mirror process.
+    /// </param>
+    public MirrorAttribute(string category = "") {
+        Category = category;
+    }
+}
